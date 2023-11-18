0f677fe665d51491b2de8350b5e2f32c93d1e56b
Igroshka <86719527+Igroshka@users.noreply.github.com>
2023-11-18 14:07:05 +0700

    Update model-config.tsx

--- app/components/model-config.tsx
+++ app/components/model-config.tsx
@@ -76,7 +76,7 @@ export function ModelConfigList(props: {
         <input
           type="number"
           min={100}
-          max={1000000}
+          max={512000}
           value={props.modelConfig.max_tokens}
           onChange={(e) =>
             props.updateConfig(
